30a838e8d1091f92f8e95ff27311692a7dbc90e2
Fábio Assunção <fabio23gt@gmail.com>
2018-09-26 01:03:28 -0300

    Novos helpers e ajustes no input timestamp

--- resources/views/_components/fields/form/timestamp.blade.php
+++ resources/views/_components/fields/form/timestamp.blade.php
@@ -1,6 +1,13 @@
+<?php
+    $outputFormat = '%Y-%m-%d %H:%M:%S';
+    if (property_exists($options, 'format_edit')) {
+        $outputFormat = $options->format_edit;
+    }
+?>
+
 <input
     @if($row->required == 1) required @endif
     type="datetime"
     class="form-control datepicker"
     name="{{ $row->field }}"
-    value="@if(isset($dataTypeContent->{$row->field})){{ \Carbon\Carbon::parse(old($row->field, $dataTypeContent->{$row->field}))->format('m/d/Y g:i A') }}@else{{old($row->field)}}@endif">
+    value="@if(isset($dataTypeContent->{$row->field})){{ \Carbon\Carbon::parse(old($row->field, $dataTypeContent->{$row->field}))->formatLocalized($outputFormat) }}@else{{old($row->field)}}@endif">
--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -57,3 +57,38 @@ if (!function_exists('form_fields')) {
         ])->render();
     }
 }
+
+if (!function_exists('delete_bread')) {
+    function delete_bread($name)
+    {
+        $dataType = \Versatile\Core\Models\DataType::where('name', $name)->first();
+        if (is_null($dataType)) {
+            return;
+        }
+
+        // Delete Translations, if present
+        if (is_bread_translatable($dataType)) {
+            $dataType->deleteAttributeTranslations($dataType->getTranslatableAttributes());
+        }
+
+        if (!is_null($dataType)) {
+            \Versatile\Core\Models\Permission::removeFrom($dataType->name);
+        }
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table($dataType->name)->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
+        $menu = \Versatile\Core\Models\MenuItem::where('title', $dataType->display_name_plural)->delete();
+
+        \Versatile\Core\Models\DataType::destroy($dataType->id);
+    }
+}
+
+if (!function_exists('convert_date_format')) {
+    function convert_date_format($date, $inputFormat = 'd/m/Y H:i:s', $outputFormat = 'Y-m-d H:i:s')
+    {
+        $date = \Carbon\Carbon::createFromFormat($inputFormat, $date);
+        return $date->format($outputFormat);
+    }
+}
