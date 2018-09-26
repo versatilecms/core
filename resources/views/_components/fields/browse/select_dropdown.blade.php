ab4826e6c02c6498f228813d5c1fc10d3f3b617d
Fábio Assunção <fabio23gt@gmail.com>
2018-09-25 21:44:18 -0300

    Substituição do 'or' pela verificação com 'isset' nas views. Necessário ára suporte com Laravel 5.7

--- resources/views/_components/fields/browse/select_dropdown.blade.php
+++ resources/views/_components/fields/browse/select_dropdown.blade.php
@@ -3,7 +3,7 @@
     @if($data->{$row->field . '_page_slug'})
         <a href="{{ $data->{$row->field . '_page_slug'} }}">{!! $options->options->{$data->{$row->field}} !!}</a>
     @else
-        {!! $options->options->{$data->{$row->field}} or '' !!}
+        {!! isset($options->options->{$data->{$row->field}}) ?  $options->options->{$data->{$row->field}} : '' !!}
     @endif
 
 @else
