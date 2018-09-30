9c59f8e38db5562ee979901a729e92cc56accd2a
Fábio Assunção <fabio23gt@gmail.com>
2018-09-30 18:13:12 -0300

    Atualização do form type text_area

--- resources/views/_components/fields/form/text_area.blade.php
+++ resources/views/_components/fields/form/text_area.blade.php
@@ -1,13 +1,15 @@
+<?php
+if(isset($dataTypeContent->{$row->field})) {
+        $fieldValue = old($row->field, $dataTypeContent->{$row->field});
+} elseif(isset($options->default)) {
+        $fieldValue = old($row->field, $options->default);
+} else {
+        $fieldValue = old($row->field);
+}
+?>
+
 <textarea
         @if($row->required == 1) required @endif
         class="form-control"
         name="{{ $row->field }}"
-        rows="{{ isset($options->display->rows) ? $options->display->rows : 5 }}">
-        @if(isset($dataTypeContent->{$row->field}))
-            {{ old($row->field, $dataTypeContent->{$row->field}) }}
-        @elseif(isset($options->default))
-            {{ old($row->field, $options->default) }}
-        @else
-            {{ old($row->field) }}
-        @endif
-</textarea>
+        rows="{{ isset($options->display->rows) ? $options->display->rows : 5 }}">{{ $fieldValue }}</textarea>
